31c6029e594cc704f8c6657603af70faf3ef0182
Abdelrahman Abdalla <ayabdall@ualberta.ca>
2021-09-24 16:16:34 -0600

    added screenshots

--- server.py
+++ server.py
@@ -2,7 +2,7 @@
 import socketserver
 from os import error, listdir, path
 
-# Copyright 2013 Abram Hindle, Eddie Antonio Santos
+# Copyright 2013 Abram Hindle, Eddie Antonio Santos, Ay Abdall
 # 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
